4e211ad15fd50bfd7716e5a875443199f3986979
Victor Palma <vicpal25@yahoo.com>
2023-07-11 12:52:18 -0700

    Adding categories

--- main.ts
+++ main.ts
@@ -22,101 +22,87 @@ app.use(cors())
 // Parse JSON bodies
 app.use(bodyParser.json())
 
-app.get('/categories/', async (req, res) => {
+app.get("/categories/", async (req, res) => {
   try {
     const recommendations = {
       hits: {
         hits: [
           {
             _source: {
-              item_id: 'motortrend-article-news-98a0384e-aed4-432a-bb1e-cf59f5811e23',
-              content: {
-                primary: 'article',
-                secondary: 'news'
-              },
-              event: {
-                type: 'click'
-              },
-              attributes: {
-                article: {
-                  id: '98a0384e-aed4-432a-bb1e-cf59f5811e23',
-                  title: 'TVR Announces Comeback with Gordon Murray Design, Cosworth V-8 Power',
-                  authors: 'Eric Weiner'
+              source: "youtube",
+              topics: [
+                {
+                  metrics: 1400000,
+                  id: "98a0384e-aed4-432a-bb1e-cf59f5811e23",
+                  name: "The Corvette Z06 is the best sounding american car",
                 },
-                session_id: '-2692120873218701822'
-              },
-              user_id: '3430023021207935000',
-              timestamp: '2023-06-13T21:11:40.004Z'
-            }
+                {
+                  metrics: 1136378,
+                  id: "46691b36-85bc-4ec9-9c1b-410370f63c69",
+                  name: "1,100HP Lucid Air v tuned BMW M3: Drag Race",
+                },
+                {
+                  metrics: 731234,
+                  id: "9c2ac983-5409-42d8-8578-6b1c74449b94",
+                  name: "FIRST DRIVE: Rolls-Royce Spectre - 576bhp, £330k Electric Masterpiece | Top Gear",
+                },
+              ],
+            },
           },
           {
             _source: {
-              item_id: 'motortrend-article-news-46691b36-85bc-4ec9-9c1b-410370f63c69',
-              content: {
-                primary: 'article',
-                secondary: 'news'
-              },
-              event: {
-                type: 'click'
-              },
-              attributes: {
-                article: {
-                  id: '46691b36-85bc-4ec9-9c1b-410370f63c69',
-                  title: 'Toyota Teases Iconic Land Cruiser&#x27;s Likely Return to U.S.',
-                  authors: 'Justin Westbrook,Alisa Priddle,Manufacturer'
+              source: "reddit",
+              topics: [
+                {
+                  metrics: 2282,
+                  id: "98a0384e-aed4-432a-bb1e-cf59f5811e23",
+                  name: "Lexus LC500",
                 },
-                session_id: '-388885061026897551'
-              },
-              user_id: '6313679408649969000',
-              timestamp: '2023-06-13T21:12:24.175Z'
-            }
+                {
+                  metrics: 1609,
+                  id: "46691b36-85bc-4ec9-9c1b-410370f63c69",
+                  name: "2023 Audi R8 GT vs The Cheapest Audi R8 You Can Buy",
+                },
+                {
+                  metrics: 1490,
+                  id: "9c2ac983-5409-42d8-8578-6b1c74449b94",
+                  name: "2022 Subaru BRZ",
+                },
+                {
+                  metrics: 1397,
+                  id: "9c2ac983-5409-42d8-8578-6b1c74449b92",
+                  name: "2019 Ferrari 488 Pista",
+                },
+                {
+                  metrics: 1103,
+                  id: "9c2ac983-5409-42d8-8578-6b1c74449b90",
+                  name: "Lamborghini Miura Paradise",
+                },
+              ],
+            },
           },
-          {
-            _source: {
-              item_id: 'motortrend-article-reviews-9c2ac983-5409-42d8-8578-6b1c74449b94',
-              content: {
-                primary: 'article',
-                secondary: 'reviews'
-              },
-              event: {
-                type: 'click'
-              },
-              attributes: {
-                article: {
-                  id: '9c2ac983-5409-42d8-8578-6b1c74449b94',
-                  title: '2023 Honda Pilot vs. 2024 Mazda CX-90: Head vs. Heart in This 3-Row Battle',
-                  authors: 'Scott Evans,Brandon Lim'
-                },
-                session_id: '-6206744200385971269'
-              },
-              user_id: '6815719862573843000',
-              timestamp: '2023-06-13T21:13:06.838Z'
-            }
-          }
-        ]
-      }
-    }
-
-    const hits = recommendations.hits.hits
-
+        ],
+      },
+    };
+​
+    const hits = recommendations.hits.hits;
+​
     const response = hits.map((hit) => {
-      const sourceItem = hit._source
+      const sourceItem = hit._source;
       return {
-        item_id: sourceItem.item_id,
-        content: sourceItem.content,
-        event: sourceItem.event,
-        attributes: sourceItem.attributes,
-        user_id: sourceItem.user_id,
-        timestamp: sourceItem.timestamp
-      }
-    })
-
-    res.status(200).json(response)
+        source: sourceItem.source,
+        topics: sourceItem.topics,
+      };
+    });
+​
+    res.status(200).json(response);
   } catch (error) {
-    console.error('Error retrieving categories:', error)
-    res.status(500).json({ message: 'An error occurred while retrieving categories.' })
+    console.error("Error retrieving categories:", error);
+    res
+      .status(500)
+      .json({ message: "An error occurred while retrieving categories." });
   }
-})
+});
 
 app.post('/generate', async (req, res) => {
   const { topic } = req.body
